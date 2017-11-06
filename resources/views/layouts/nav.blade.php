7466552144c30bb6aed739357256fac9ead25b77
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-11-06 10:42:30 +0200

    edit navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,20 +1,38 @@
-<div class="blog-masthead">
-    <div class="container">
-        <nav class="nav">
-            <a class="nav-link active" href="#">Home</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <a class="navbar-brand" href="/">News Blog</a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Categories
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="#">Government</a>
                     <a class="dropdown-item" href="#">Media</a>
                     <a class="dropdown-item" href="#">Weather</a>
                     <a class="dropdown-item" href="#">Sports</a>
                     <a class="dropdown-item" href="#">Technology</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Analytics</a>
                 </div>
-            </div>
-        </nav>
+            </li>
+
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </ul>
+
+        <form class="form-inline">
+            <button class="btn btn-outline-success" type="button">Login</button>
+            <button class="btn btn-outline-success" type="button">Sign Up</button>
+        </form>
     </div>
-</div>
+</nav>
